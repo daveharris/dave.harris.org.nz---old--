9ec9d83cbf1a4f0cb38035758b64899e23fb7965
Dave Harris <dave@harris.org.nz>
2011-03-02 14:08:07 +1300

    Rails app creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DaveHarrisOrgNz::Application.config.secret_token = 'c3af7bf98243ce0afdbe9ebf7f1f87fec50c7ed76e26e0ca471bea5ca8536c248261bd837d2b29191ab2247ff5daa6526e6dfd11fe23b31ac3f4c065c54dba8f'
